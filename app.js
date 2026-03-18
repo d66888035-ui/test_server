3782e891f768b210a9c5336f1dfd7167dfcad8c2
Gulika921 <gulika921@gmail.com>
2026-03-18 13:57:07 +0100

    Update server log to include API URL

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 const app = express();
+const API_URL = "http://192.168.178.79:3000";
 
 app.get("/", (req, res) => {
   res.send("Hello from Raspberry Pi 🚀");
 });
 
 app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running");
+  console.log(`Server running on ${API_URL}`);
 });
