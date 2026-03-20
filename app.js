00721baeec43da46be880a953de99bef582e2beb
gulika921@gmail.com <gulika921@gmail.com>
2026-03-20 13:05:24 +0100

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const URL = "100.86.121.25";
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Raspberry Pi 🚀");
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Raspberry Pi 🚀" });
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on ${URL}:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
