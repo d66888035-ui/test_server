43fbc08cb88ac2f87ed1ab108610f2b22272ce17
gulika921@gmail.com <gulika921@gmail.com>
2026-03-30 23:03:43 +0200

    Testing

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "app",
-      script: "./start.cjs",
+      script: "./app.js",
       cwd: "/home/apisit/test_server/backend",
       interpreter: "node",
       watch: false,
